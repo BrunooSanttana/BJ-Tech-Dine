91fbec490aaa537948005b219dd7d4c0b393904b
Bruno Santana <brunoliveiraq@gmail.com>
2025-05-12 15:16:11 -0300

    [feat] css and js for faturamento page

--- src/pages/Faturamento.js
+++ src/pages/Faturamento.js
@@ -1,47 +1,38 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Importando Link
+import { Link } from 'react-router-dom';
 import logo from '../images/presleylogo.png';
-
+import './Faturamento.css'; // Importa o CSS
 
 const Faturamento = () => {
-  const [startDate, setStartDate] = useState(''); // Data de início
-  const [endDate, setEndDate] = useState(''); // Data de fim
-  const [totalAmount, setTotalAmount] = useState(0); // Total das vendas
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [totalAmount, setTotalAmount] = useState(0);
 
-  // Função para buscar o faturamento com base no período selecionado
   const fetchFaturamento = async () => {
     if (!startDate || !endDate) {
       alert('Por favor, selecione o período.');
       return;
     }
-
     try {
       const response = await fetch(`http://localhost:5000/faturamento?startDate=${startDate}&endDate=${endDate}`);
-      if (!response.ok) {
-        throw new Error('Erro na rede: ' + response.statusText);
-      }
+      if (!response.ok) throw new Error('Erro na rede: ' + response.statusText);
       const data = await response.json();
-
-      // Defina apenas o total retornado
       setTotalAmount(data.total);
-
-      console.log('Faturamento encontrado:', data); // Para depuração
     } catch (error) {
       console.error('Erro ao buscar faturamento:', error);
     }
   };
 
   return (
-    <div>
-      {/* Logo como link para o Menu */}
+    <div className="faturamento-container">
       <Link to="/Menu">
-        <img src={logo} alt="Logo"  style={{ cursor: 'pointer', width: '100px', marginBottom: '20px' }} />
+        <img src={logo} alt="Logo" className="faturamento-logo" />
       </Link>
 
-      <h2>Faturamento</h2>
+      <h2 className="faturamento-title">Faturamento</h2>
 
       <div>
-        <label>
+        <label className="faturamento-label">
           Data de Início:
           <input
             type="date"
@@ -52,7 +43,7 @@ const Faturamento = () => {
       </div>
 
       <div>
-        <label>
+        <label className="faturamento-label">
           Data de Fim:
           <input
             type="date"
@@ -62,9 +53,9 @@ const Faturamento = () => {
         </label>
       </div>
 
-      <button onClick={fetchFaturamento}>Buscar Faturamento</button>
+      <button onClick={fetchFaturamento} className="faturamento-button">Buscar Faturamento</button>
 
-      <h3>Total Faturado no Período: R${totalAmount.toFixed(2)}</h3>
+      <h3 className="faturamento-total">Total Faturado no Período: R${totalAmount.toFixed(2)}</h3>
     </div>
   );
 };
